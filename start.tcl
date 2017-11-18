350dac0e77d4152f5e38289be4691c97ff51c6d0
Andres Rios <rianby64@gmail.com>
2017-11-18 16:52:02 +0300

    Defina de la forma mas simple una tarea

--- start.tcl
+++ start.tcl
@@ -11,6 +11,11 @@ proc howmanymonths { d1 d2 } {
   return $i
 }
 
+proc render'task { gantt task } {
+  upvar $task t
+  $gantt task $t(description) $t(start) $t(end) 100
+}
+
 set project_start [clock scan "2004-01-01 00:00:00" -format {%Y-%m-%d %H:%M:%S}]
 set project_end [clock scan "2006-04-01 00:00:00" -format {%Y-%m-%d %H:%M:%S}]
 set months [howmanymonths $project_start $project_end]
@@ -19,19 +24,21 @@ set rows 20
 canvas .c -width [expr {40 * $months}] -height [expr {20 * $rows}]
 pack .c
 
-
-set s [::Plotchart::createGanttchart .c \
+set gantt [::Plotchart::createGanttchart .c \
   [clock format $project_start -format {%d %B %Y}] \
   [clock format $project_end -format {%d %B %Y}] 20]
 
 set current_time $project_start
 while { $current_time < $project_end } {
-  $s vertline [clock format $current_time -format {%d %b}] \
+  $gantt vertline [clock format $current_time -format {%d %b}] \
     [clock format $current_time -format {%d %B %Y}]
   set current_time [clock add $current_time 1 months]
 }
 
-$s title "Administrador de tiempos (version inicial)"
-
-set spring [$s task "5.1.3 Realizacion..." "1 march 2004" "1 june 2004" 100]
-
+$gantt title "Administrador de tiempos (version inicial)"
+array set t1 {
+  description "Tarea 1 por hacer"
+  start "1 march 2004"
+  end "1 june 2004"
+}
+render'task $gantt t1

ed2f2c19fe6561befc45cb5aa0d7230d0abc5b34
Andres Rios <rianby64@gmail.com>
2017-11-18 16:41:10 +0300

    Ponga el alto del canvas en funcion del numero de entradas

--- start.tcl
+++ start.tcl
@@ -14,27 +14,22 @@ proc howmanymonths { d1 d2 } {
 set project_start [clock scan "2004-01-01 00:00:00" -format {%Y-%m-%d %H:%M:%S}]
 set project_end [clock scan "2006-04-01 00:00:00" -format {%Y-%m-%d %H:%M:%S}]
 set months [howmanymonths $project_start $project_end]
+set rows 20
 
-canvas .c -width [expr {40 * $months}] -height 200
+canvas .c -width [expr {40 * $months}] -height [expr {20 * $rows}]
 pack .c
 
 
 set s [::Plotchart::createGanttchart .c \
   [clock format $project_start -format {%d %B %Y}] \
-  [clock format $project_end -format {%d %B %Y}] 10]
+  [clock format $project_end -format {%d %B %Y}] 20]
 
 set current_time $project_start
-set i 1
 while { $current_time < $project_end } {
-  #puts [clock format $current_time -format {%Y-%m-%d %H:%M:%S}]
   $s vertline [clock format $current_time -format {%d %b}] \
     [clock format $current_time -format {%d %B %Y}]
-  set current_time [clock add $current_time $i months]
+  set current_time [clock add $current_time 1 months]
 }
-puts $i
-#for { set i 1 } { $i < 10 } { incr i } {
-#  puts [clock format [clock add $t $i months] -format {%Y-%m-%d %H:%M:%S}]
-#}
 
 $s title "Administrador de tiempos (version inicial)"
 

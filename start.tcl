7cab2ec7b450cb3fd131c569ddbdc8153c87fcf5
Andres Rios <rianby64@gmail.com>
2017-11-23 11:27:32 +0300

    Reorganice un poco el codigo

--- start.tcl
+++ start.tcl
@@ -329,61 +329,84 @@ namespace eval tareas {
   }
 }
 
+array set t0 {
+  id 1
+  keynote "5"
+  description "Projecto 5"
+  expand 1
+}
 array set t1 {
+  id 2
+  keynote "5.1"
+  description "Preliminares"
+  expand 1
+}
+array set t2 {
   id 3
   keynote "5.1.7"
   description "Tarea 1 por hacer"
   start "2004-02-05"
   end "2004-02-25"
   connectWith 4
+  expand 0
 }
-array set t2 {
+array set t3 {
   id 4
   keynote "5.1.8"
   description "Tarea 2 por hacer"
   start "2004-03-04"
   end "2004-03-15"
+  expand 0
 }
-array set t3 {
+
+array set t4 {
   id 5
-  keynote "5.1.9"
+  keynote "5.2"
+  description "Cimentaciones"
+  expand 1
+}
+array set t6 {
+  id 6
+  keynote "5.2.9"
   description "Tarea 3 por hacer"
   start "2004-04-04"
   end "2004-05-15"
-  connectWith 6
+  connectWith 7
+  expand 0
 }
-array set t4 {
-  id 6
-  keynote "5.1.9"
+array set t7 {
+  id 7
+  keynote "5.2.10"
   description "Tarea 3 por hacer"
   start "2004-05-04"
   end "2004-06-15"
+  expand 0
 }
 
 set path .c
 pack [button .btn -text "Go"]
 set gantt [tareas::init $path "2004-02-01 00:00:00" "2004-07-01 00:00:00"]
 pack $path
-tareas::render'task $gantt t1
 tareas::render'task $gantt t2
+tareas::render'task $gantt t3
 
-set sumario1 [$gantt summary "Primer sumario" \
-  [dict get $tareas::tasks(3) task] [dict get $tareas::tasks(4) task]]
+#set sumario1 [$gantt summary "Primer sumario" \
+#  [dict get $tareas::tasks(3) task] [dict get $tareas::tasks(4) task]]
 #puts $sumario1
 
-tareas::render'task $gantt t3
-tareas::render'task $gantt t4
+tareas::render'task $gantt t6
+tareas::render'task $gantt t7
 
-set sumario2 [$gantt summary "Segundo sumario" \
-  [dict get $tareas::tasks(5) task] [dict get $tareas::tasks(6) task]]
+#set sumario2 [$gantt summary "Segundo sumario" \
+#  [dict get $tareas::tasks(5) task] [dict get $tareas::tasks(6) task]]
 #puts $sumario2
 
 tareas::render'connections $gantt
 bind .c <<UpdateTask>> [list muestremelo %W $gantt %d]
 bind .btn <1> [list modifique'la'tarea $path $gantt]
 
-$gantt connect [list 0 [lindex $sumario1 1] [lindex $sumario1 1] 0] \
-  [list 0 [lindex $sumario2 1] [lindex $sumario2 1] 0]
+#$gantt connect [list 0 [lindex $sumario1 1] [lindex $sumario1 1] 0] \
+#  [list 0 [lindex $sumario2 1] [lindex $sumario2 1] 0]
 
 #parray tareas::tasks
 
@@ -397,7 +420,7 @@ proc modifique'la'tarea { path gantt } {
   dict set tarea3 payload end "2004-05-15"
   dict set tarea3 payload description "Otra tarea por modificar"
   dict set tarea3 payload keynote "5.1.11"
-  puts $tareas::tasks(3)
   set tareas::tasks(3) $tarea3
   tareas::redraw'task $path $gantt 3
+  puts $tareas::tasks(3)
 }

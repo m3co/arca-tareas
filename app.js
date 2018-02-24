1303c7c5bc4290695dc4521025e5d79d1f76f266
Andres Rios <rianby64@gmail.com>
2018-02-24 12:30:51 +0300

    Move the fetching data to an example json file

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
 'use strict';
 (() => {
-
-var taski = [
-{"start":new Date("Sun Dec 09 01:36:45"),"end":new Date("Sun Dec 09 02:36:45"),"id": "1", "description":"E Job"},
-{"start":new Date("Sun Dec 09 04:56:32"),"end":new Date("Sun Dec 09 06:35:47"),"id": "2", "description":"A Job"},
-{"start":new Date("Sun Dec 09 06:29:53"),"end":new Date("Sun Dec 09 06:34:04"),"id": "3", "description":"D Job"},
-{"start":new Date("Sun Dec 09 05:35:21"),"end":new Date("Sun Dec 09 06:21:22"),"id": "4", "description":"P Job"},
-{"start":new Date("Sun Dec 09 05:00:06"),"end":new Date("Sun Dec 09 05:05:07"),"id": "5", "description":"D Job"},
-{"start":new Date("Sun Dec 09 03:46:59"),"end":new Date("Sun Dec 09 04:54:19"),"id": "6", "description":"P Job"},
-{"start":new Date("Sun Dec 09 04:02:45"),"end":new Date("Sun Dec 09 04:48:56"),"id": "7", "description":"N Job"},
-{"start":new Date("Sun Dec 09 03:27:35"),"end":new Date("Sun Dec 09 03:58:43"),"id": "8", "description":"E Job"},
-{"start":new Date("Sun Dec 09 01:40:11"),"end":new Date("Sun Dec 09 03:26:35"),"id": "9", "description":"A Job"},
-{"start":new Date("Sun Dec 09 03:00:03"),"end":new Date("Sun Dec 09 03:09:51"),"id": "10", "description":"D Job"},
-{"start":new Date("Sun Dec 09 01:21:00"),"end":new Date("Sun Dec 09 02:51:42"),"id": "11", "description":"P Job"},
-{"start":new Date("Sun Dec 09 01:08:42"),"end":new Date("Sun Dec 09 01:33:42"),"id": "12", "description":"N Job"},
-{"start":new Date("Sun Dec 09 00:27:15"),"end":new Date("Sun Dec 09 00:54:56"),"id": "13", "description":"E Job"},
-{"start":new Date("Sun Dec 09 00:29:48"),"end":new Date("Sun Dec 09 00:44:50"),"id": "14", "description":"D Job"},
-{"start":new Date("Sun Dec 09 07:39:21"),"end":new Date("Sun Dec 09 07:43:22"),"id": "15", "description":"P Job"},
-{"start":new Date("Sun Dec 09 07:00:06"),"end":new Date("Sun Dec 09 07:05:07"),"id": "16", "description":"D Job"},
-{"start":new Date("Sun Dec 09 08:46:59"),"end":new Date("Sun Dec 09 09:54:19"),"id": "17", "description":"P Job"},
-{"start":new Date("Sun Dec 09 09:02:45"),"end":new Date("Sun Dec 09 09:48:56"),"id": "18", "description":"N Job"},
-{"start":new Date("Sun Dec 09 08:27:35"),"end":new Date("Sun Dec 09 08:58:43"),"id": "19", "description":"E Job"},
-{"start":new Date("Sun Dec 09 08:40:11"),"end":new Date("Sun Dec 09 08:46:35"),"id": "20", "description":"A Job"},
-{"start":new Date("Sun Dec 09 08:00:03"),"end":new Date("Sun Dec 09 08:09:51"),"id": "21", "description":"D Job"},
-{"start":new Date("Sun Dec 09 10:21:00"),"end":new Date("Sun Dec 09 10:51:42"),"id": "22", "description":"P Job"},
-{"start":new Date("Sun Dec 09 11:08:42"),"end":new Date("Sun Dec 09 11:33:42"),"id": "23", "description":"N Job"},
-{"start":new Date("Sun Dec 09 12:27:15"),"end":new Date("Sun Dec 09 12:54:56"),"id": "24", "description":"E Job"},
-{"start":new Date("Sat Dec 08 23:12:24"),"end":new Date("Sun Dec 09 10:26:13"),"id": "25", "description":"A Job"}];
-
-  render(taski);
+  fetch('example.json')
+    .then(response => response.json())
+    .then(tasks => {
+      render(tasks.map(d => {
+        d.start = new Date(d.start);
+        d.end = new Date(d.end);
+        return d;
+      }));
+    });
 function render(tasks) {
   var tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")

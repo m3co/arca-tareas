4d1401f9cf32b014461b1c9fad825a7dc048cc3d
Andres Rios <rianby64@gmail.com>
2018-03-10 12:07:33 +0300

    Mueva el servidor static al middleware

--- middleware.js
+++ middleware.js
@@ -5,6 +5,7 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 const app = express();
 const config = Ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+const { exec } = require('child_process');
 
 const wsProxy = proxy('/socket.io', config.socketio);
 app.use(wsProxy);
@@ -15,3 +16,4 @@ app.use('/', proxy(config.static));
 console.log(`Listening to http://localhost:${config.port}`);
 const server = app.listen(Number(config.port));
 server.on('upgrade', wsProxy.upgrade);
+exec(`http-server -c-1 -p ${config.static.port} ./static`);

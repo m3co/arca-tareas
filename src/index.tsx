a9a9c28c453e66c64af2aba8cb17d312286b8150
Novikov Anton <anton.novikov.y@gmail.com>
2019-11-03 10:46:00 +0300

    Move initialization store to separate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { ARCASocket, reducer } from 'arca-redux';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import 'typeface-roboto';
 import App from './App/App';
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION__: any;
-  }
-}
-
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-const socket = new ARCASocket(store);
+import { store, socket } from './redux/store';
 
 render(
   <Provider store={store}>

96e4208ce28c5eaa6631c9b5481eeb0aa91a7473
Novikov Anton <anton.novikov.y@gmail.com>
2019-11-08 15:32:14 +0300

    Add getDateList util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,15 @@ export const sortByEnd = (rows: State['Source']['AAU-Tasks-Gantt']['Rows']) => r
 export const getDurationTaskInDays = (row: State['Source']['AAU-Tasks-Gantt']['Rows'][0]) => {
   return ((Date.parse(String(row.End)) - Date.parse(String(row.Start))) / 60000) / 1440;
 }
+
+export const getDateList = (start: Date, end: Date) => {
+  const list = new Array();
+  const date = new Date(start);
+
+  while (date <= end) {
+      list.push(new Date(date));
+      date.setDate(date.getDate() + 1);
+  }
+
+  return list;
+}
